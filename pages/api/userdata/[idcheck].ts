78f65a534467ac4afbba92f30aab3589b161ddc2
LJW <golove010@gmail.com>
2022-10-11 22:26:45 +0900

    change type

--- pages/api/userdata/[idcheck].ts
+++ pages/api/userdata/[idcheck].ts
@@ -43,7 +43,7 @@ export default async function Handler(
             ps,
             email,
             name,
-            hp,
+            hp: hp || " ",
             ymd,
             purpose,
           },
